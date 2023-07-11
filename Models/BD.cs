374ab7fbe93c7ee265237704d6345733c587fb0a
roni <47867214@ort>
2023-07-11 17:01:20 -0300

    cambios BD

--- Models/BD.cs
+++ Models/BD.cs
@@ -27,15 +27,22 @@ static class BD{
         Partido MiPartido=null;
         using (SqlConnection db= new SqlConnection(_connectionString)){
             string SQL="SELECT * FROM Partido WHERE IdPartido=@Partido";
-            MiPartido=db.QueryFirstOrDeult<Partido>(SQL, new{Partido=idPartido});
+            MiPartido=db.QueryFirstOrDefault<Partido>(SQL, new{Partido=idPartido});
         }
         return MiPartido;
        }
         public static Candidato VerInfoCandidato(int idCandidato)
-          {}        
-        public static ListarPartidos()
-
-            {}
+        {
+        Candidato MiCandidato=null;
+        using (SqlConnection db= new SqlConnection(_connectionString)){
+            string SQL="SELECT * FROM Candidato WHERE IdCandidato=@Candidato";
+            MiCandidato=db.QueryFirstOrDefault<Candidato>(SQL, new{Candidato=idCandidato});
+             }
+             return MiCandidato;
+        }        
+        public static ListarPartidos(){
+            
+        }
         public static ListarCandidatos(int idPartido)
             {}
 
